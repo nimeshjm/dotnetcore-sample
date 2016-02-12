d5ec55d9fe9b55972c469f5e0547c8517e9ebf1c
Nimesh Manmohanlal <nimeshjm@gmail.com>
2016-02-12 17:09:03 +0000

    Add JS support via bower

--- src/helloworld/Startup.cs
+++ src/helloworld/Startup.cs
@@ -16,7 +16,9 @@ namespace helloworld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) => { await context.Response.WriteAsync($"Hello World: {context.Request.Path}"); });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            //app.Run(async (context) => { await context.Response.WriteAsync($"Hello World: {context.Request.Path}"); });
         }
 
         // Entry point for the application.
---
+++ src/helloworld/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿(function() {
+    var $widebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function() {
+        $widebarAndWrapper.toggleClass("hide-sidebar");
+        if ($widebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
